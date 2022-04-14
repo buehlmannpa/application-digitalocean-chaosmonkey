8ded5e17d26b744391da3bfb155313bf6cbd0c9f
Patrick Bühlmann <>
2022-04-14 11:32:35 +0200

    Added code for the backup of the logfile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,11 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 ### Added
+- Added code into the backup script to create a backup from the existing logfile [@buehlmannpa](https://github.com/buehlmannpa).
 - Added an empty Readme where the repository will be described [@buehlmannpa](https://github.com/buehlmannpa).
 - Added functionality to eliminate pods and log to a defined logfile [@buehlmannpa](https://github.com/buehlmannpa).
 - Added the functions to read and interact with the config inputs [@buehlmannpa](https://github.com/buehlmannpa).
 
 ### Changed
+- Changed the mechanism to prof if the pod is younger than an hour [@buehlmannpa](https://github.com/buehlmannpa).
 - Changed the code where deleting the namespaces defined in the config-file from the namespaces on the k8s cluster [@buehlmannpa](https://github.com/buehlmannpa).
 
 ### Deleted
--- backup.sh
+++ backup.sh
@@ -10,3 +10,46 @@
 #   license         GNU General Public License
 #
 #----------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------
+# var declaration
+#----------------------------------------------------------------------------------------------
+LOG_DIR="/data/chaos-monkey"
+BACKUP_DIR="/data/chaos-monkey/backup"
+DOWNLOAD_DIR="/home/chaosmonkey/download"
+CURRENT_DATE=`date +%Y%m%d`
+
+CRONJOB_NAME="backup_cron"
+BACKUP_TIME="23 23 * * *" #23:23
+
+
+#----------------------------------------------------------------------------------------------
+# Configure backup job (once at startup)
+#----------------------------------------------------------------------------------------------
+if [[ ! -f "$LOG_DIR/backup_ready" ]];
+then
+    echo "$BACKUP_TIME $HOME_DIR/backup.sh" >> $DOWNLOAD_DIR/$CRONJOB_NAME
+    crontab $DOWNLOAD_DIR/$CRONJOB_NAME
+    rm $DOWNLOAD_DIR/$CRONJOB_NAME
+
+    touch $LOG_DIR/backup_ready
+else
+    echo "Backup is already initialized"
+
+
+#----------------------------------------------------------------------------------------------
+# Backup the current logfile (every day)
+#----------------------------------------------------------------------------------------------
+    #chaosmonkey-log-<date>.txt
+
+    # copy the current logfile into the backup folder
+    cp $LOG_DIR/chaosmonkey-log.txt $BACKUP_DIR/chaosmonkey-log-$CURRENT_DATE.txt
+
+    # copy the backup-logfile to the backup host over scp (ssh connect doesn't work at the moment)
+    ####### scp <datei> buehlmannpa@195.88.87.171:/home/buehlmannpa
+
+    # clear the current logfile(s)
+    echo "" > $LOG_DIR/chaosmonkey-log.txt
+    echo "" > $LOG_DIR/chaosmonkey-log-color.txt
+
+fi
